65ec4c842a4545ff4421b9ed11afa9f6889534f1
Young Joong Kim <yjkimjunior@berkeley.edu>
2016-05-29 19:12:26 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	entry: './main.js',
+	output: {
+		path: './',
+		filename: './index.js'
+	},
+	devServer: {
+		inline: true,
+		port: 8000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: '/node_modules/',
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+
+	}
+}
+
